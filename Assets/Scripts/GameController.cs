a57e5630dcb5ffb8a412d6c025ea18c383067ecb
Yaron <s94298314049@gmail.com>
2023-02-04 11:27:41 +0800

    Revert "Revert "增加勝利判斷""
    This reverts commit 173c3e16eb04e5dc22ea80b659c6fce19d6c1b13.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,13 +8,16 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
     
+    //音樂
+    public AudioSource my_audio;
+    public AudioClip ac_End;
+    
     //計時器
     public int m_Seconds,m_Min,m_Sec;//總秒數,分鐘,秒
     public Text m_Timer;//時間文字呈現
+    public Text timeUp;//時間到
     
-    //場上剩下蘿蔔
-    public GameObject radish;
-    public static string howHaveRadish;
+    //場上蘿蔔
     public int maxRadish;
     
     //遊戲開始
@@ -23,14 +26,22 @@ public class GameController : MonoBehaviour
     //玩家分數
     public Text txt_P1, txt_P2;
     public int int_P1, int_P2;
+    
+    //結算圖及文字
+    public Text howWin;
+    public Image p1, p2;
+    public Sprite p1_win, p2_win;
+    
     //結束畫面
     public GameObject end;
 
     public void Start()
     {
+        my_audio = GetComponent<AudioSource>();
         gameStart = true;
         end.SetActive(false);
         StartCoroutine("CountDown");
+        
     }
     
     
@@ -41,9 +52,9 @@ public class GameController : MonoBehaviour
             int i = int_P1 + int_P2;
             if (i==maxRadish)
               {
-                       StopCoroutine("CountDown");
-                       End();
-                       gameStart = false;
+                StopCoroutine("CountDown");
+                End();
+                gameStart = false;
               }
         }
        
@@ -74,19 +85,21 @@ public class GameController : MonoBehaviour
             m_Timer.text = string.Format("{0}:{1}", m_Min.ToString("00"), m_Sec.ToString("00"));
         }
         yield return new WaitForSeconds(1);
+        timeUp.text = "TIME'S UP!!!!";
+        yield return new WaitForSeconds(1);
         End();
     }
     
     //誰得分數
-    public void RadishFraction()
+    public void RadishFraction(string who,int fraction)
     {
-        switch (howHaveRadish)
+        switch (who)
         {
-            case  "P1":
-                int_P1 += 1;
+            case "P1":
+                int_P1 += fraction;
                 break;
-            case  "P2" :
-                int_P2 += 1;
+            case "P2":
+                int_P2 += fraction;
                 break;
         }
     }
@@ -94,11 +107,34 @@ public class GameController : MonoBehaviour
     //結算畫面
     public void End()
     {
+        my_audio.clip= ac_End;
+        my_audio.Play();
+        WinGame();
         txt_P1.text = int_P1.ToString();
         txt_P2.text = int_P2.ToString();
         end.SetActive(true);
     }
-
+    
+    //勝利判斷
+    public void WinGame()
+    {
+        if (int_P1 > int_P2)
+        {
+            howWin.text = "P1 WIN!!!!";
+            p1.sprite = p1_win;
+        }
+        else if(int_P2>int_P1)
+        {
+            howWin.text = "P2 WIN!!!!";
+            p2.sprite = p2_win;
+        }
+        else
+        {
+            howWin.text = "Tie";
+        }
+        
+    }
+  
     //按鈕控制
     public void Reset()
     {
