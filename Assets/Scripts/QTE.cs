580043fdb556eaa0a04c79a25e756c707ca2540e
Kirin <kirinchess@gmail.com>
2023-02-05 14:26:47 +0800

    1.QTE修正

--- Assets/Scripts/QTE.cs
+++ Assets/Scripts/QTE.cs
@@ -69,7 +69,6 @@ public class QTE : MonoBehaviour
     {
         AudioManagerScript.Instance.PlayAudioClip("qte_success");
         callback(true);
-        // hitterRotate.Kill();
         hitter.transform.rotation = Quaternion.Euler(0, 0, 0);
 
         hitBox.transform.DOScale(new Vector3(1.3f,1.3f,1),info.failShakeTime).OnComplete(()=> {
